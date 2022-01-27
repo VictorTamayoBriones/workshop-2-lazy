82fd0349ea1c1c5a8027d3acdca13fb1bcbf38c1
VictorTamayoBriones <victortamayo509@gmail.com>
2022-01-27 13:43:02 -0600

    api random foxes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import axios from 'axios';
 
-console.log('Happy hacking :)')
+const btnAdd = document.querySelector('#add');
+const container = document.querySelector('#images');
+
+const randomNumber = () => Math.floor(Math.random() * 124 );
+
+const noImagesText = () => {
+    const message = document.createElement('h3');
+    message.textContent = 'No hay imagenes cargadas';
+    message.setAttribute('class', 'message');
+    document.body.appendChild(message);
+}
+
+const createImage = (image) => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image);
+    container.appendChild(newImage);
+}
+
+const getImage = async () =>{
+    const res = await axios.get(`https://randomfox.ca/images/${randomNumber()}.jpg`)
+    const image = (res.request.responseURL);
+    createImage(image);
+    verifyImages();
+}
+
+const verifyImages = ()=>{
+    const images = document.querySelectorAll('img');
+    const message = document.querySelector('.message');
+
+    images.length === 0 ? noImagesText() : message ? document.body.removeChild(message) : null;
+}
+
+btnAdd.addEventListener('click', getImage);
+
+verifyImages();
